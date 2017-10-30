0ada880d6d6d14c2c8ad589b9790d0ccef6d86ec
toutoudnf <toutoudnf@gmail.com>
2017-10-30 17:44:23 +0800

    补充

--- mysql/connection_clean.sql
+++ mysql/connection_clean.sql
@@ -6,3 +6,4 @@ mysql> source /tmp/a.txt;
 
 # 其中，where 查询条件是可以替换的，比如换成 host like '192.168.4.1%' 就变成清理该主机发起的所有conn
 # 注意 kill 支持 option 选择是 kill 连接还是 kill query，格式如：KILL [CONNECTION | QUERY] processlist_id
+# and 上面这个操作需要拥有 FILE 权限
