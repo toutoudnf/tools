d3d0fe61fd322c5297afe9600428f88abbaebf22
toutoudnf <toutoudnf@gmail.com>
2017-10-30 17:29:57 +0800

    增加db清理conn脚本

---
+++ mysql/connection_clean.sql
@@ -0,0 +1,8 @@
+# 用于清理 MySQL 服务器端连接过多的情况
+# 参考的是：https://serverfault.com/questions/11357/bulk-or-mass-killing-misbehaving-mysql-queries
+
+mysql> SELECT concat('KILL ',id,';') FROM information_schema.processlist WHERE user='root' INTO OUTFILE '/tmp/a.txt';
+mysql> source /tmp/a.txt;
+
+# 其中，where 查询条件是可以替换的，比如换成 host like '192.168.4.1%' 就变成清理该主机发起的所有conn
+# 注意 kill 支持 option 选择是 kill 连接还是 kill query，格式如：KILL [CONNECTION | QUERY] processlist_id
